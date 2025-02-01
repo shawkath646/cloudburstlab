ddc4b5b94d8ae0063b252c6c904ae7593b7c2739
Shawkat Hossain Maruf <shawkath646@gmail.com>
2025-02-01 13:09:10 +0600

    Fix gmail api function -2

--- src/app/secured/page.tsx
+++ src/app/secured/page.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+import { resetGmailApiKey } from "@/actions/secure";
+
 export default async function Page() {
     return (
         <section>
-            SECURE Route
+            <p>SECURE Route</p>
+            <Link href={resetGmailApiKey}>Reset Gmail API Key</Link>
         </section>
     );
 }
